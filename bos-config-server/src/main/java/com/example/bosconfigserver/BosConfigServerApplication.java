d303b41d9315fa453a957527d4abf25bfac9644d
18276545776 <318717449@qq.com>
2019-03-16 13:29:48 +0800

    config server client

---
+++ bos-config-server/src/main/java/com/example/bosconfigserver/BosConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bosconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BosConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BosConfigServerApplication.class, args);
+    }
+
+}
